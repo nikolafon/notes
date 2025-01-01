8e9ceb080a8321e13f2dcd577e4b82b9c4f69dff
Nikola Nikolic <nikola.nikolic@outlook.com>
2025-01-01 23:16:02 +0100

    fixed login bug

--- notes-service/src/main/java/com/example/notes/service/UserDetailsService.java
+++ notes-service/src/main/java/com/example/notes/service/UserDetailsService.java
@@ -1,5 +1,6 @@
 package com.example.notes.service;
 
+import com.example.notes.tenant.TenantHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,7 +14,8 @@ public class UserDetailsService implements org.springframework.security.core.use
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userService.find(String.format("{ username: '%s' }", username)).stream().findFirst()
+        String tenantId = TenantHolder.getCurrentTenantId();
+        return userService.find(String.format("{ username: '%s', tenantId: %s }", username, tenantId != null ? String.format("'%s'", tenantId) : null)).stream().findFirst()
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
